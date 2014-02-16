d1be573ce7d9c20e35bfbf067d31e372d9e4db60
aniversarioperu <aniversarioperu1@gmail.com>
2014-02-16 10:53:02 +0200

    functiont to do retuits

--- find_and_retuit.py
+++ find_and_retuit.py
@@ -6,10 +6,14 @@
 For each jail, find tweets inside and retweet
 """
 import codecs
+import requests
 import config
 import os.path
 import dataset
 import lib
+import api
+
+oauth = api.get_oauth()
 
 db = dataset.connect("sqlite:///" + os.path.join(config.local_folder, "tuits.db"))
 
@@ -17,6 +21,7 @@ f = codecs.open(os.path.join(config.local_folder, "carceles_limites.csv"))
 data = f.readlines()
 f.close()
 
+retuits = []
 for i in data:
     i = i.strip()
     if i.startswith("Carcel,"):
@@ -35,5 +40,18 @@ for i in data:
         res = db.query("select * from tuits where carcel='" + penal +"' and retuited='no'")
         for i in res:
             if lib.tuit_inside_jail(i['status_id'], poly) == True:
-                print unicode(i)
+                url = "https://api.twitter.com/1.1/statuses/retweet/"
+                url += str(i['status_id']) + ".json"
+                try:
+                    r = requests.post(url=url, auth=oauth)
+                    print "Retuited %i" % i['status_id']
+                    retuits.append(i['status_id'])
+                except:
+                    print "Error", r.text
+
+table = db['tuits']
+for i in retuits:
+    data = dict(status_id=i, retuited="yes")
+    print data
+    table.update(data, ['status_id'])
 
